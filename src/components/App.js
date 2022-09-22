91b3c0796f6b6545aa6e994693616d6dc1830a94
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-09-22 10:47:19 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import NavBar from "./NavBar";
-import Home from "./Home";
+ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+import userEvent from "@testing-library/user-event";
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name}
+        city={user.city}
+        color={user.color} />
+      <About
+        bio={user.bio}
+        user={user}
+
+      />
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+
+import React from "react"
+const Links = (links) => {
+
+    return <>
+        <h3>Links</h3>
+< a href ={links.github}>https://github.com/liza</a>
+<a href ={links.linkedin}>https://www.linkedin.com/in/liza/</a>
+
+
+
+
+    </>
+}
+export default Links;
